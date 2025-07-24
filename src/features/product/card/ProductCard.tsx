ec6391d3449ca1fb4ff58fc89fb63bb48422ca7a
Kirill <49115928+kirill-efimov-sk@users.noreply.github.com>
2025-07-25 00:56:04 +0300

    fix: card style, default button props, product card

--- src/features/product/card/ProductCard.tsx
+++ src/features/product/card/ProductCard.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Card } from '../../../shared/card';
 import { AddToCart } from '../../cart/buttons/addToCart';
+import { truncatingText } from '../../../utils/variablesFormatting';
 import { BaseCardProps, CardImageProps } from 'src/shared/card/card.types';
 
 export interface ProductCardProps extends BaseCardProps, Required<CardImageProps> {}
 
 export const ProductCard: FC<ProductCardProps> = ({ price, image, name, description }): React.JSX.Element => {
-  const truncatedText = description.length > 100 ? description.substring(0, 100) + '<...>' : description;
+  const truncatedText = useMemo(() => truncatingText(description, 100), [description]);
+
   return (
     <Card price={price} image={image} name={name} description={truncatedText}>
       <AddToCart count={0} />
